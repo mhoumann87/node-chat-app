1079dd1904123a6280e1a12ce750a2de0e7ed2e6
Michael Houmann <michael@michael-h.dk>
2017-11-17 13:52:06 +0100

    Added express and server.js

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+let app = express();
+let port = 3000;
+const publicPath = path.join(__dirname, '../public');
+
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is started at port ${port}`);
+});
+
