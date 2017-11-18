35de930c3e474c133fe40ab950c41ed08a452b5f
Michael Houmann <michael@michael-h.dk>
2017-11-18 17:07:43 +0100

    Added CSS for chatpage

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
 let socket = io();
 
-socket.on('connect', function() {
+socket.on('connect', function () {
   console.log('Connected to server');
 });
 
-socket.on('disconnect', function() {
+socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message) {
+socket.on('newMessage', function (message) {
   console.log('New Message: ', message);
 
   let li = jQuery('<li></li>');
@@ -17,7 +17,7 @@ socket.on('newMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
-socket.on('newLocationMessage', function(message) {
+socket.on('newLocationMessage', function (message) {
   let li = jQuery('<li></li>');
   let a = jQuery('<a target="_blank">My current location</a>');
 
@@ -28,29 +28,35 @@ socket.on('newLocationMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function(e) {
+jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+
+  let messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
-  }, function() {
-
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('');
   });
 });
 
 let locationButton = jQuery('#send-location');
-locationButton.on('click', function() {
-  if(!navigator.geolocation) {
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
 
-  navigator.geolocation.getCurrentPosition(function(position) {
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
-  }, function() {
-    alert('Unable to getch location');
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location');
   });
 });
